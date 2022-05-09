bc2a907704d8673710594613c6e951a435aeb07d
PietroSF <pietrosgfoltran@gmail.com>
2022-05-09 09:25:27 -0300

    finally passing all tests

--- src/main.js
+++ src/main.js
@@ -1,77 +1,65 @@
-/* eslint-disable no-unused-expressions */
-const hotelList = {
-  lakewood: {
+const hotels = [
+  {
     name: "Lakewood",
     rating: 3,
     regular: [110, 90],
-    reward: [80, 80],
+    rewards: [80, 80],
   },
-  bridgewood: {
+  {
     name: "Bridgewood",
     rating: 4,
     regular: [160, 60],
-    reward: [110, 50],
+    rewards: [110, 50],
   },
-  ridgewood: {
+  {
     name: "Ridgewood",
     rating: 5,
     regular: [220, 150],
-    reward: [100, 40],
+    rewards: [100, 40],
   },
-};
+];
 
-function computeRates(bDay, wkndDay) {
-  // pass values/call from parseDates()
-  // must get client type to choose correct key/value pair
-  // then iterate hotels, calculate rate and return in an ary of objects
-
-  /* Hotel {
-      name: "name",
-      totalRate: rate,
-      rating: rating
-  },
-    etc. */
-
-  // sort by highest rating, then by cheapest total rate
-  // then return name of first obj in ary, done
+function getRates(weekDays, guestPlan) {
+  return hotels.map((hotel) => {
+    const sumRates = 
+    hotel[guestPlan][0] * weekDays[0] + 
+    hotel[guestPlan][1] * weekDays[1];
+    return [hotel.name, sumRates, hotel.rating];
+  });
 }
 
-function parseDates(weekDays) {
-  // iterate through dates array
-  // can use includes to check whether dates are a business day or weekend day
+function bestCheapestHotel(finalRates) {
+  const minRate = finalRates.sort((a, b) => a[1] - b[1])[0][1];
+  const cheapestHotels = finalRates.filter((hotel) => hotel[1] === minRate);
+  return cheapestHotels.sort((a, b) => b[2] - a[2])[0][0];
+}
 
-  // increment counter for each type of day
-  // call next function from within here or return the counter and call
+function dayCount(weekDays) {
+  const businessDays = ["mon", "tues", "wed", "thur", "fri"];
+  const weekEnd = ["sat", "sun"];
+  let dayTypeCounter = [0, 0];
+  weekDays.map((str) => {
+    businessDays.map((day) => {
+      str.includes(day) ? (dayTypeCounter[0] += 1) : null;
+    });
+    weekEnd.map((day) => {
+      str.includes(day) ? (dayTypeCounter[1] += 1) : null;
+    });
+  });
+  return dayTypeCounter;
+}
+
+function splitDate(input) {
+  return dayCount(input.split(":")[1].split(","));
 }
 
 function getCheapestHotel(input) {
-  return "Cheapest hotel name";
-  // split input at colon to get client type and dates
-  // split dates at commas to get an array
-
-  // call parseDates() with the array of dates as parameter
-
-  /* 
-  GET input ()
-
-  PARSE client type, date string ~~> can be done in main function
-
-  PARSE day of the week in date string ~~> needs to be parsed into
-  business days and weekend days, might need its own function
-
-  READ client type, FLAG client type ~~> can probably be done succintly
-  and included in another function
-
-  READ day type, FLAG day type ~~> this and the last instruction are
-  a good fit to share a function
-
-  DETERMINE cost
-  SETTLE ties with rating ~~> as before these two probably are a good fit
-  for a single function
-
-  RETURN best cheapest hotel ~~> tests require this be done in the main 
-  function
-  */
+  const guestPlan = input.split(":")[0].toLowerCase();
+  /* getRates() takes guestPlan and splitDate(), splitDate() calls 
+     dayCount, which returns array with a counter for business days and weekend
+     days. getRates computes rates for each hotel and returns an array,
+     which is sorted in bestCheapestHotel(). */
+  return bestCheapestHotel(getRates(splitDate(input), guestPlan));
 }
 
-// exports.getCheapestHotel = getCheapestHotel;
+exports.getCheapestHotel = getCheapestHotel;
